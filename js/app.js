f27c0b1fd1524e260e593bd9a0a05cd32bf9234d
johan-flowcode <moises.escudero.os@gmail.com>
2024-01-23 18:39:59 +0100

    modificacion de carpetas  y nuevos archivos zombie.

---
+++ js/app.js
@@ -0,0 +1,26 @@
+
+window.onload = function() {
+  const scene = document.querySelector('a-scene');
+  const numeroDeCopias = 5; // Número de copias de cada modelo
+
+  for (let i = 0; i < numeroDeCopias; i++) {
+    ['modelo-3d', 'zombieW', 'zombieR'].forEach(modelo => {
+      let personaje = document.createElement('a-entity');
+      personaje.setAttribute('gltf-model', `#${modelo}`);
+      personaje.setAttribute('animation-mixer', '');
+
+      let x = Math.random() * 30 - 10; // Ajusta estos valores según tu escena
+      let y = 0; // Asumiendo que están en el suelo
+      let z = Math.random() * 30 - 20;
+      personaje.setAttribute('position', `${x} ${y} ${z}`);
+
+      scene.appendChild(personaje);
+
+      // Agregar luces puntuales para cada personaje
+      let luz = document.createElement('a-entity');
+      luz.setAttribute('light', 'type: point; intensity: 0.5; distance: 5; color: #FFF');
+      luz.setAttribute('position', `${x} ${y + 2} ${z}`);
+      scene.appendChild(luz);
+    });
+  }
+};
