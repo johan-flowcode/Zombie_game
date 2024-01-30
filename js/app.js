b6bf37d8288bd6e3d2bf0c7a36038bae275b7393
johan-flowcode <moises.escudero.os@gmail.com>
2024-01-30 22:35:03 +0100

    modificacion en archivos html,js creacion de archivo css nivel.html.

--- js/app.js
+++ js/app.js
@@ -29,12 +29,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
   let score = 0
 
-  const numeroDehongos = 8
+  const numeroDehongos = 4
   for (let i = 0; i < numeroDehongos; i++) {
     ['hongo'].forEach(modelo => {
       let hongos = document.createElement('a-entity');
       hongos.setAttribute('png-model', `#${modelo}`);
-      hongos.setAttribute('geometry', { primitive: 'sphere', radius: '0.2'});
+      hongos.setAttribute('geometry', { primitive: 'sphere', radius: '0.2' });
       hongos.setAttribute('material', { shader: 'flat', src: '#hongo' });
       hongos.setAttribute('animation', 'property: rotation; to: 360 360 0; loop: true; dur: 30000; easing: linear');
       hongos.setAttribute('shootable', '');
@@ -62,12 +62,35 @@ document.addEventListener('DOMContentLoaded', function () {
         score++;
         const scoreText = document.querySelector('#score-text');
         if (scoreText) {
-          scoreText.setAttribute('value', `Honguitos: ${score}`);
+          scoreText.setAttribute('value', `Score: ${score}`);
+        }
+        if (score === numeroDehongos) {
+          mostrarMensajeFinJuego();
         }
       });
     }
   });
+
+
+  function mostrarMensajeFinJuego() {
+    console.log("Intentando mostrar el mensaje de fin de juego");
+    const mensajeFinJuego = document.getElementById('fin-juego-mensaje');
+    mensajeFinJuego.style.display = 'block'; // Cambia a 'block' para mostrar
+  }
+
+  // Función para ocultar el mensaje del juego
+  function ocultarMensajeFinJuego() {
+    const mensajeFinJuego = document.getElementById('fin-juego-mensaje');
+    mensajeFinJuego.style.display = 'none'; // Cambia a 'none' para ocultar
+  }
+
+  document.getElementById('next-level-button').addEventListener('click', function () {
+    window.location.href = 'nivel.html';
+  });
+
+
 });
+
 
 
 
